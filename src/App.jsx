255d79578b1965d96594e48796d4a4817b3810bc
Priyanka Patel <100508950+PriyankaPatel01@users.noreply.github.com>
2024-07-03 11:38:39 +0000

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Priyanka Patel",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "priyankarpatel192000@gmail.com",
+  gitHub: "PriyankaPatel01",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "priyanka-patel-5b809524a",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
